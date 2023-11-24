2468192791a7868c0d052881f83783c95d79e481
louaila <lou591@hotmail.fr>
2023-11-24 16:44:17 +0100

    ok

---
+++ includes/_footer.php
@@ -0,0 +1,41 @@
+
+<footer>
+        <section>
+            <div>
+                <img src="/img/logoSoeur.png" alt="logo Soeureinement">
+                <p class="firstli">Soeureinement</p>
+            </div>
+            <ul>
+                <li class="firstLiFooter">Nos randonnées<a href="#"></a></li>
+                <li>Facile<a href="#"></a></li>
+                <li>Moyen<a href="#"></a></li>
+                <li>Difficile<a href="#"></a></li>
+            </ul>
+            <ul>
+                <li class="firstLiFooter">Nos conseils <a href="#"></a></li>
+                <li>Equipements<a href="#"></a></li>
+                <li>Santé<a href="#"></a></li>
+                <li>Tous les conseils<a href="#"></a></li>
+            </ul>
+            <ul>
+                <li class="firstLiFooter">Soeureinement<a href="#"></a></li>
+                <li>Contact<a href="#"></a></li>
+                <li>Se connecter<a href="#"></a></li>
+                <li>S'inscrire<a href="#"></a></li>
+            </ul>
+            <ul id="NousSuivre">
+                <li class="firstLiFooter" >Nous suivre</li>
+                <li><img  id=logoinsta src="/icones/icoInsta.png" alt=""><a href="#"></a></li>
+            </ul>
+        </section>
+        <section>
+            <ul>
+                <li><a href="#"></a>Mentions légales</li>
+                <li><a href="#"></a>CGU</li>
+                <li><a href="#"></a>Design by Hidden figures</li>
+                <li>
+                    <img id="logoshoes" src="/icones/shoes.png" alt=""> <a href="#"></a>
+                </li>
+            </ul>
+        </section>
+    </footer>
---
+++ includes/_header.php
@@ -0,0 +1,16 @@
+<header>
+        <div>       
+            <img src="/img/logoSoeur.png" alt="logo Soeureinement"> 
+            <p>soeureinement</p>
+        </div>
+            <nav>
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Nos randonnées</a></li>
+                    <li><a href="#">Conseils</a></li>
+                    <li>|</li>
+                    <li><a href="#Connexion"><img src="/icones/icoConex.png" alt="#Connexion"></a></li>
+                </ul>
+                
+            </nav>
+    </header>
